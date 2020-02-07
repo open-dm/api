fbca187ebd516dfd8a677b6d68fce2b0d2290e7b
George Broadhurst <george@rapanuiclothing.com>
2020-02-07 21:45:53 +0000

    refactor: make Armor work for all Characters

--- app/Models/Armor/Armor.php
+++ app/Models/Armor/Armor.php
@@ -3,23 +3,23 @@
 namespace App\Models\Armor;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Characters\Monster;
+use App\Models\Characters\Character;
 
 class Armor extends Model
 {
     protected $table = 'armor';
 
-    public function getArmorClass(Monster $monster) {
+    public function getArmorClass(Character $character) {
         $armor_class = $this->base_armor;
 
         if ($this->apply_dex_modifier) {
             if ($this->dex_cap) {
                 $armor_class += min(
                     $this->dex_cap,
-                    $monster->dexterity_modifier
+                    $character->dexterity_modifier
                 );
             } else {
-                $armor_class += $monster->dexterity_modifier;
+                $armor_class += $character->dexterity_modifier;
             }
         }
 
