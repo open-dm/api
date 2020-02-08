bc5ba6610b3d68e445ad79e5b974b7313a232e7f
DanEvans485 <dan.e@teemill.com>
2020-02-08 23:18:24 +0000

    doc: todo comment

--- app/Http/Requests/ApiCreateRequest.php
+++ app/Http/Requests/ApiCreateRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
+// TODO - Some thought needs to go into how this is going to work for diff models.
+
 class ApiCreateRequest extends FormRequest
 {
     public function rules()
