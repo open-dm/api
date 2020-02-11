7d8521e546b1246c600f14d049d82d633da5aa6a
DanEvans485 <dan.e@teemill.com>
2020-02-11 20:03:48 +0000

    feat: added character seeder

---
+++ database/seeds/CharacterSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Models\Characters\Monster;
+use App\Models\Characters\Player;
+use App\Models\Core\Alignment;
+use App\Models\Core\Dice;
+use App\Models\Core\Size;
+use Illuminate\Database\Seeder;
+
+class CharacterSeeder extends Seeder
+{
+    public function run()
+    {
+        $character = new Monster([
+        'name' => 'Orc',
+        'base_hp' => 15,
+        'speed' => 30,
+        'strength' => 14,
+        'dexterity' => 14,
+        'constitution' => 14,
+        'intelligence' => 14,
+        'wisdom' => 14,
+        'charisma' => 14,
+        'hp_dice_count' => 2,
+        'is_template' => true,
+    ]);
+
+        $character->size()->associate(Size::findByCode('large'));
+        $character->alignment()->associate(Alignment::findByCode('chaotic_evil'));
+        $character->hp_dice()->associate(Dice::find(4));
+
+        $character->save();
+
+
+
+        $character = new Player([
+            'name' => 'New Player',
+            'base_hp' => 15,
+            'speed' => 30,
+            'strength' => 14,
+            'dexterity' => 14,
+            'constitution' => 14,
+            'intelligence' => 14,
+            'wisdom' => 14,
+            'charisma' => 14,
+            'hp_dice_count' => 2,
+            'is_template' => true,
+        ]);
+
+        $character->size()->associate(Size::findByCode('large'));
+        $character->alignment()->associate(Alignment::findByCode('chaotic_evil'));
+        $character->hp_dice()->associate(Dice::find(4));
+
+        $character->save();
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             LanguageSeeder::class,
             AlignmentSeeder::class,
             DamageTypeSeeder::class,
+            CharacterSeeder::class,
         ]);
     }
 }
