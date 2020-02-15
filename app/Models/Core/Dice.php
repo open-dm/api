ee0af462e674453155f2f0a4ef9feb4e8e9811f9
DanEvans485 <dan.e@teemill.com>
2020-02-15 16:41:57 +0000

    chore: added model find trait

--- app/Models/Core/Dice.php
+++ app/Models/Core/Dice.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\Core;
 
+use App\Traits\ModelFindByTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Dice extends Model
 {
+    use ModelFindByTrait;
+
     protected $table = 'dice';
 
     public $timestamps = false;
--- app/Models/Core/Race.php
+++ app/Models/Core/Race.php
@@ -2,10 +2,13 @@
 
 namespace App\Models\Core;
 
+use App\Traits\ModelFindByTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Race extends Model
 {
+    use ModelFindByTrait;
+
     protected $table = 'races';
 
     public $timestamps = false;
