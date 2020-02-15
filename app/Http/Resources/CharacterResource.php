f997986d419ead236a07abb239be1844fec3ea6a
DanEvans485 <dan.e@teemill.com>
2020-02-15 16:43:08 +0000

    feat: added model api resources

---
+++ app/Http/Resources/CharacterResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CharacterResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id'   => $this->id,
+            'name' => $this->name,
+
+            'attributes' => new AbilitiesResource($this),
+
+            'size'      => new SizeResource($this->size),
+            'race'      => new RaceResource($this->race),
+            'alignment' => new AlignmentResource($this->alignment),
+
+//            'parent' => $this->parent->id,
+
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
