581958659861ec11cc44c52062cb04a5b4864fd6
George Broadhurst <george@rapanuiclothing.com>
2020-02-15 15:39:43 +0000

    feat: correct orc values and seed an owlbear

--- database/seeds/CharacterSeeder.php
+++ database/seeds/CharacterSeeder.php
@@ -6,6 +6,8 @@ use App\Models\Core\Alignment;
 use App\Models\Core\Challenge;
 use App\Models\Core\Dice;
 use App\Models\Core\Size;
+use App\Models\Items\Item;
+use App\Models\Language\Language;
 use Illuminate\Database\Seeder;
 
 class CharacterSeeder extends Seeder
@@ -14,26 +16,54 @@ class CharacterSeeder extends Seeder
     {
         $character = new Monster([
             'name' => 'Orc',
-            'base_hp' => 15,
+            'base_hp' => 6,
             'speed' => 30,
-            'strength' => 14,
-            'dexterity' => 14,
-            'constitution' => 14,
-            'intelligence' => 14,
-            'wisdom' => 14,
-            'charisma' => 14,
+            'strength' => 16,
+            'dexterity' => 12,
+            'constitution' => 16,
+            'intelligence' => 7,
+            'wisdom' => 11,
+            'charisma' => 10,
             'hp_dice_count' => 2,
             'is_template' => true,
         ]);
 
         $character->size()->associate(Size::findByCode('medium'));
         $character->alignment()->associate(Alignment::findByCode('chaotic_evil'));
-        $character->hp_dice()->associate(Dice::find(4));
+        $character->hp_dice()->associate(Dice::find(3));
         $character->challenge()->associate(Challenge::find(4));
 
         $character->save();
 
+        $character->items()->attach([
+            Item::firstWhere('name', 'Hide')->id,
+            Item::firstWhere('name', 'Great Axe')->id,
+            Item::firstWhere('name', 'Javelin')->id
+        ], ['equipped' => true]);
+        $character->languages()->attach([
+            1,
+            8
+        ]);
 
+        $character = new Monster([
+            'name' => 'Owlbear',
+            'base_hp' => 21,
+            'speed' => 40,
+            'strength' => 20,
+            'dexterity' => 12,
+            'constitution' => 17,
+            'intelligence' => 3,
+            'wisdom' => 12,
+            'charisma' => 7,
+            'hp_dice_count' => 7,
+            'is_template' => true,
+        ]);
+
+        $character->size()->associate(Size::findByCode('large'));
+        $character->hp_dice()->associate(Dice::find(4));
+        $character->challenge()->associate(Challenge::find(7));
+
+        $character->save();
 
         $character = new Player([
             'name' => 'New Player',
