5dde4ef72e37ac236160f03fccbc7e0f2011b526
DanEvans485 <dan.e@teemill.com>
2020-02-27 22:40:55 +0000

    feat: added orFail to ModelFindByTrait.php

--- app/Traits/ModelFindByTrait.php
+++ app/Traits/ModelFindByTrait.php
@@ -5,11 +5,23 @@ trait ModelFindByTrait
 {
     public static function findBy(string $field, $value)
     {
-        return static::where($field, $value)->first();
+        return static::where($field, $value)
+            ->first();
+    }
+
+    public static function findByOrFail(string $field, $value)
+    {
+        return static::where($field, $value)
+            ->firstOrFail();
     }
 
     public static function findByCode($value)
     {
         return static::findBy('code', $value);
+    }
+
+    public static function findByCodeOrFail($value)
+    {
+        return static::findByOrFail('code', $value);
     }
 }
