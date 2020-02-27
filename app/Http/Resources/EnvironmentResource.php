ae1ca5f5d61a7d4703050ca064569ccb5357b820
DanEvans485 <dan.e@teemill.com>
2020-02-27 22:44:44 +0000

    feat: environment resource

---
+++ app/Http/Resources/EnvironmentResource.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EnvironmentResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'code' => $this->code,
+        ];
+    }
+}
