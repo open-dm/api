ef086ac5103473281776e64cc2c6f7f1c8d1a247
DanEvans485 <dan.e@teemill.com>
2020-02-27 22:39:34 +0000

    feat: added Arr extension methods

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Closure;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,5 +27,53 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        Arr::macro(
+            'map',
+            function (array $array, Closure $callback) {
+                return array_map(
+                    $callback,
+                    $array,
+                    array_keys($array)
+                );
+            }
+        );
+
+        Arr::macro(
+            'values',
+            function (array $array) {
+                return array_values($array);
+            }
+        );
+
+        Arr::macro(
+            'keys',
+            function (array $array) {
+                return array_keys($array);
+            }
+        );
+
+        Arr::macro(
+            'flip',
+            function (array $array) {
+                return array_flip($array);
+            }
+        );
+
+        Arr::macro(
+            'each',
+            function (array $array, Closure $callback) {
+                foreach ($array as $item => $index) {
+                    $callback($item, $index);
+                }
+            }
+        );
+
+        Arr::macro(
+            'merge',
+            function (array ...$arrays) {
+                return array_merge(...$arrays);
+            }
+        );
     }
 }
